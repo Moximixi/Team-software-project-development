d1886d8d8dd70deac203d325d5b2b4c2223439a5
linWeiZhou <1587387052@qq.com>
2019-04-23 21:11:16 +0800

    林伟洲：添加查询选课界面（包括三个子界面）

--- MyDemo.java
+++ MyDemo.java
@@ -1,37 +1,44 @@
-import java.awt.CardLayout;
 import java.awt.EventQueue;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.FlowLayout;
 
 import javax.swing.JFrame;
-import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JPanel;
-import javax.swing.JTextPane;
-import javax.swing.JTable;
+import javax.swing.JMenu;
+import java.awt.CardLayout;
 import java.awt.Color;
-import javax.swing.table.DefaultTableModel;
-import java.awt.SystemColor;
-import java.awt.Rectangle;
-import java.awt.Canvas;
-import java.awt.Font;
-import java.awt.FlowLayout;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JMenuItem;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JTextPane;
 import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableModel;
 
+import java.awt.Font;
+import java.awt.Rectangle;
+import java.awt.SystemColor;
 
-//2222222222222222222222222222222222222222222222222222222222
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class MyDemo {
 
 	private JFrame frame;
-	private JPanel panel_courseSelect;
-	private JPanel panel_trainPlan;
-	private JPanel panel_document;
+	private JPanel panel_search;
 	private JPanel panel_userInfo;
+	private JPanel panel_logOut;
+	private JTextField textField_courseName;
 	private JTable table;
 	private JTable trainpan_table;
-	
+	private JTextField textField_courseNum;
+	private JTextField textField_courseMajor;
+	private JTextField textField_courseCollege;
+	private JTable table_1;
+	private JTable table_2;
+
 	/**
 	 * Launch the application.
 	 */
@@ -68,12 +75,192 @@ public class MyDemo {
 		panel.setLayout(new CardLayout(0, 0));
 		
 		JPanel panel_homePage = new JPanel();
-		panel_homePage.setForeground(new Color(0, 0, 255));
 		panel.add(panel_homePage);
 		
+		JPanel panel_querySocres = new JPanel();
+		panel.add(panel_querySocres);
+		
+		//选课
+		JPanel panel_courseSelect_queryCourse = new JPanel();
+		panel.add(panel_courseSelect_queryCourse);
+		panel_courseSelect_queryCourse.setLayout(null);
+		
+		JTextPane textPane_courseName = new JTextPane();
+		textPane_courseName.setFont(new Font("宋体", Font.BOLD, 20));
+		textPane_courseName.setText("课程名称：");
+		textPane_courseName.setBounds(100, 79, 116, 34);
+		panel_courseSelect_queryCourse.add(textPane_courseName);
+		
+		textField_courseName = new JTextField();
+		textField_courseName.setBounds(220, 79, 112, 33);
+		panel_courseSelect_queryCourse.add(textField_courseName);
+		textField_courseName.setColumns(10);
+		
+		JTextPane textPane_courseNum = new JTextPane();
+		textPane_courseNum.setText("课程编号：");
+		textPane_courseNum.setFont(new Font("宋体", Font.BOLD, 20));
+		textPane_courseNum.setBounds(460, 80, 116, 34);
+		panel_courseSelect_queryCourse.add(textPane_courseNum);
+		
+		textField_courseNum = new JTextField();
+		textField_courseNum.setColumns(10);
+		textField_courseNum.setBounds(580, 80, 112, 33);
+		panel_courseSelect_queryCourse.add(textField_courseNum);
+		
+		JTextPane textPane_courseMajor = new JTextPane();
+		textPane_courseMajor.setText("所属专业：");
+		textPane_courseMajor.setFont(new Font("宋体", Font.BOLD, 20));
+		textPane_courseMajor.setBounds(460, 142, 116, 34);
+		panel_courseSelect_queryCourse.add(textPane_courseMajor);
+		
+		textField_courseMajor = new JTextField();
+		textField_courseMajor.setColumns(10);
+		textField_courseMajor.setBounds(580, 142, 112, 33);
+		panel_courseSelect_queryCourse.add(textField_courseMajor);
+		
+		JTextPane textPane_courseCollege = new JTextPane();
+		textPane_courseCollege.setText("所属学院：");
+		textPane_courseCollege.setFont(new Font("宋体", Font.BOLD, 20));
+		textPane_courseCollege.setBounds(102, 147, 116, 34);
+		panel_courseSelect_queryCourse.add(textPane_courseCollege);
+		
+		textField_courseCollege = new JTextField();
+		textField_courseCollege.setColumns(10);
+		textField_courseCollege.setBounds(222, 147, 112, 33);
+		panel_courseSelect_queryCourse.add(textField_courseCollege);
+		
+		JButton button_cancel = new JButton("清除条件");
+		button_cancel.setFont(new Font("宋体", Font.BOLD, 20));
+		button_cancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		button_cancel.setBounds(151, 278, 181, 48);
+		panel_courseSelect_queryCourse.add(button_cancel);
+		
+		JButton button_query = new JButton("查询");
+		button_query.setFont(new Font("宋体", Font.BOLD, 20));
+		button_query.setBounds(460, 278, 181, 48);
+		panel_courseSelect_queryCourse.add(button_query);
+		
+		JPanel panel_courseSelect_courseList = new JPanel();
+		panel.add(panel_courseSelect_courseList);
+		panel_courseSelect_courseList.setLayout(null);
+		
+		JButton button = new JButton("选课");
+		button.setBounds(91, 109, 72, 27);
+		panel_courseSelect_courseList.add(button);
+		
+		JButton button_1 = new JButton("选课");
+		button_1.setBounds(91, 149, 72, 27);
+		panel_courseSelect_courseList.add(button_1);
+		
+		JButton button_2 = new JButton("选课");
+		button_2.setBounds(91, 189, 72, 27);
+		panel_courseSelect_courseList.add(button_2);
+		
+		JButton button_3 = new JButton("选课");
+		button_3.setBounds(91, 229, 72, 27);
+		panel_courseSelect_courseList.add(button_3);
+		
+		table_1 = new JTable();
+		table_1.setFont(new Font("宋体", Font.BOLD, 20));
+		table_1.setBorder(new LineBorder(new Color(0, 0, 0)));
+		table_1.setRowHeight(40);//指定每一行的行高40
+		
+		table_1.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"  \u72B6\u6001", "\u8BFE\u7A0B\u7F16\u53F7", "\u8BFE\u7A0B\u540D", "\u5B66\u5206", "\u65F6\u95F4", "\u5730\u70B9", "\u5907\u6CE8", "\u8003\u8BD5\u65F6\u95F4"},
+				{"", null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+			},
+			new String[] {
+				"\u72B6\u6001", "\u8BFE\u7A0B\u7F16\u53F7", "\u8BFE\u7A0B\u540D", "\u5B66\u5206", "\u65F6\u95F4", "\u5730\u70B9", "\u5907\u6CE8", "\u8003\u8BD5\u65F6\u95F4"
+			}
+		));
+		table_1.getColumnModel().getColumn(1).setResizable(false);
+		table_1.getColumnModel().getColumn(2).setResizable(false);
+		table_1.setBounds(83, 60, 722, 200);
+		panel_courseSelect_courseList.add(table_1);
+		
+		JPanel panel_courseSelect_courseSelected = new JPanel();
+		panel.add(panel_courseSelect_courseSelected);
+		panel_courseSelect_courseSelected.setLayout(null);
+		
+		JButton button_4 = new JButton("退课");
+		button_4.setBounds(91, 109, 72, 27);
+		panel_courseSelect_courseSelected.add(button_4);
+		
+		JButton button_5 = new JButton("退课");
+		button_5.setBounds(91, 149, 72, 27);
+		panel_courseSelect_courseSelected.add(button_5);
+		
+		JButton button_6 = new JButton("退课");
+		button_6.setBounds(91, 189, 72, 27);
+		panel_courseSelect_courseSelected.add(button_6);
+		
+		JButton button_7 = new JButton("退课");
+		button_7.setBounds(91, 229, 72, 27);
+		panel_courseSelect_courseSelected.add(button_7);
+		
+		table_2 = new JTable();
+		table_2.setFont(new Font("宋体", Font.BOLD, 20));
+		table_2.setBorder(new LineBorder(new Color(0, 0, 0)));
+		table_2.setRowHeight(40);//指定每一行的行高40
+		table_2.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"  \u72B6\u6001", "\u8BFE\u7A0B\u7F16\u53F7", "\u8BFE\u7A0B\u540D", "\u5B66\u5206", "\u65F6\u95F4", "\u5730\u70B9", "\u5907\u6CE8", "\u8003\u8BD5\u65F6\u95F4"},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+			},
+			new String[] {
+				"\u72B6\u6001", "\u8BFE\u7A0B\u7F16\u53F7", "\u8BFE\u7A0B\u540D", "\u5B66\u5206", "\u65F6\u95F4", "\u5730\u70B9", "\u5907\u6CE8", "\u8003\u8BD5\u65F6\u95F4"
+			}
+		));
+		table_2.setFont(new Font("宋体", Font.BOLD, 20));
+		table_2.setBounds(83, 60, 722, 200);
+		panel_courseSelect_courseSelected.add(table_2);
+		
+		// 查询选课界面的“查询”按钮
+		button_query.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				panel.removeAll();
+				panel.add(panel_courseSelect_courseList);
+				panel.updateUI();
+				//TODO:点击查询之后的数据变化
+			}
+		});
+		// 开课列表界面的“选课”按钮
+		button.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				panel.removeAll();
+				panel.add(panel_courseSelect_courseSelected);
+				panel.updateUI();
+				//TODO:点击查询之后的数据变化
+			}
+		});
+		// 我的选课界面的“退课”按钮
+		button.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				//TODO:点击查询之后的数据变化
+				
+				panel.removeAll();
+				panel.add(panel_courseSelect_courseSelected);//刷新
+				panel.updateUI();
+			}
+		});
+		
+		
+		
 		table = new JTable();
-		JPanel panel_trainPlan;
-		panel_trainPlan = new JPanel();
+		JPanel panel_trainPlan = new JPanel();
 		FlowLayout flowLayout = (FlowLayout) panel_trainPlan.getLayout();
 		flowLayout.setVgap(150);
 		panel.add(panel_trainPlan);
@@ -111,39 +298,25 @@ public class MyDemo {
 		trainpan_table.setBackground(SystemColor.textHighlightText);
 		panel_trainPlan.add(trainpan_table);
 		
-		
-		
-		JTextPane textPane = new JTextPane();
-		textPane.setText("�ҵ���ҳ");
-		panel_homePage.add(textPane);
-		
-		JPanel panel_querySocres = new JPanel();
-		panel.add(panel_querySocres);
-		
-		JTextPane textPane_1 = new JTextPane();
-		textPane_1.setText("��ѯ�ɼ�");
-		panel_querySocres.add(textPane_1);
-		
-		JPanel panel_courseSelect;
-		panel_courseSelect = new JPanel();
-		panel.add(panel_courseSelect);
-		
-		JTextPane textPane_2 = new JTextPane();
-		textPane_2.setText("ѡ��");
-		panel_courseSelect.add(textPane_2);
-		
-		JPanel panel_document;
-		panel_document = new JPanel();
+		JPanel panel_document = new JPanel();
 		panel.add(panel_document);
+		
+		JPanel panel_search;
+		panel_search = new JPanel();
+		panel.add(panel_search);
 		
 		JPanel panel_userInfo;
 		panel_userInfo = new JPanel();
 		panel.add(panel_userInfo);
 		
+		JPanel panel_logOut;
+		panel_logOut = new JPanel();
+		panel.add(panel_logOut);
+		
 		JMenuBar menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
 		
-		JMenu menu_homePage = new JMenu("\u6211\u7684\u9996\u9875");
+		JMenu menu_homePage = new JMenu("我的首页");
 		menu_homePage.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
@@ -154,41 +327,57 @@ public class MyDemo {
 		});
 		menuBar.add(menu_homePage);
 		
-		JMenu menu_querySocres = new JMenu("\u67E5\u8BE2\u6210\u7EE9");
+		JMenu menu_querySocres = new JMenu("查询成绩");
 		menu_querySocres.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseClicked(MouseEvent e) {
+			public void mouseClicked(MouseEvent arg0) {
 				panel.removeAll();
-				panel.add(panel_querySocres);
+				panel.add(panel_courseSelect_queryCourse);
 				panel.updateUI();
 			}
 		});
 		menuBar.add(menu_querySocres);
 		
-		JMenu menu_courseSelect = new JMenu("\u9009\u8BFE\u7CFB\u7EDF");
+		JMenu menu_courseSelect = new JMenu("选课系统");
 		menuBar.add(menu_courseSelect);
 		
-		JMenuItem mntmNewMenuItem = new JMenuItem("\u67E5\u8BE2\u9009\u8BFE");
-		mntmNewMenuItem.addMouseListener(new MouseAdapter() {
+		JMenuItem menuItem = new JMenuItem("查询选课");
+		menuItem.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
 				panel.removeAll();
-				panel.add(panel_courseSelect);
+				panel.add(panel_courseSelect_queryCourse);
 				panel.updateUI();
 			}
 		});
-		menu_courseSelect.add(mntmNewMenuItem);
-		
-		JMenuItem mntmNewMenuItem_1 = new JMenuItem("\u9009\u8BFE\u5217\u8868");
-		menu_courseSelect.add(mntmNewMenuItem_1);
-		
-		JMenuItem menuItem = new JMenuItem("\u6211\u7684\u9009\u8BFE");
 		menu_courseSelect.add(menuItem);
 		
-		JMenu menu_trainPlan = new JMenu("\u57F9\u517B\u65B9\u6848");
+		JMenuItem menuItem_1 = new JMenuItem("开课列表");
+		menuItem_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				panel.removeAll();
+				panel.add(panel_courseSelect_courseList);
+				panel.updateUI();
+			}
+		});
+		menu_courseSelect.add(menuItem_1);
+		
+		JMenuItem menuItem_2 = new JMenuItem("已选课程");
+		menuItem_2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				panel.removeAll();
+				panel.add(panel_courseSelect_courseSelected);
+				panel.updateUI();
+			}
+		});
+		menu_courseSelect.add(menuItem_2);
+		
+		JMenu menu_trainPlan = new JMenu("培养方案");
 		menu_trainPlan.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseClicked(MouseEvent e) {
+			public void mouseClicked(MouseEvent arg0) {
 				panel.removeAll();
 				panel.add(panel_trainPlan);
 				panel.updateUI();
@@ -196,16 +385,50 @@ public class MyDemo {
 		});
 		menuBar.add(menu_trainPlan);
 		
-		JMenu menu_document = new JMenu("\u6587\u6863\u5E94\u7528");
+		JMenu menu_document = new JMenu("文档应用");
+		menu_document.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				panel.removeAll();
+				panel.add(panel_document);
+				panel.updateUI();
+			}
+		});
 		menuBar.add(menu_document);
 		
-		JMenu menu_userInfo = new JMenu("\u7528\u6237\u4FE1\u606F");
+		JMenu menu_search = new JMenu("搜索");
+		menu_search.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				panel.removeAll();
+				panel.add(panel_search);
+				panel.updateUI();
+			}
+		});
+		menuBar.add(menu_search);
+		
+		JMenu menu_userInfo = new JMenu("用户信息管理");
+		menu_userInfo.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				panel.removeAll();
+				panel.add(panel_userInfo);
+				panel.updateUI();
+			}
+		});
 		menuBar.add(menu_userInfo);
 		
-		JMenu menu_logOut = new JMenu("\u6CE8\u9500");
+		JMenu menu_logOut = new JMenu("注销");
+		menu_logOut.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				panel.removeAll();
+				panel.add(panel_logOut);
+				panel.updateUI();
+			}
+		});
 		menuBar.add(menu_logOut);
 		frame.getContentPane().setLayout(new CardLayout(0, 0));
-		
 		
 	}
 }
