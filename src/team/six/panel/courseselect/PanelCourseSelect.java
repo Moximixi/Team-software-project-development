a217d86afb1bca1e9a1f78f32568e334de6caa70
linWeiZhou <1587387052@qq.com>
2019-05-04 22:56:10 +0800

    选课部分

--- src/team/six/panel/courseselect/PanelCourseSelect.java
+++ src/team/six/panel/courseselect/PanelCourseSelect.java
@@ -10,28 +10,41 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableModel;
 
 public class PanelCourseSelect extends JPanel {
-
-	JPanel p = new JPanel();
+	
+	//创建选项卡面板对象
+	JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+	//创建面板
+	JPanel panel_queryCourse=new JPanel();
+	JPanel panel_listCourse=new JPanel();
+	JPanel panel_selectedCourse=new JPanel();
 	
 	public PanelCourseSelect() {
 		
 		super();
 		setLayout(new CardLayout(0, 0));
-		
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);//创建选项卡面板对象
 		add(tabbedPane, "name_422260517140338");
+		
+		initPanelQueryCourse();		//初始化（添加控件）	
+		initPanelListCourse();
+		initPanelSelectedCourse();
+			
+		//将面板加入到选项卡面板对象上
+		tabbedPane.addTab("查询选课",null,panel_queryCourse,"First panel");
+		tabbedPane.addTab("开课列表",null,panel_listCourse,"Second panel");
+		tabbedPane.addTab("已选课程",null,panel_selectedCourse,"Third panel");
+		super.setSize(800, 470);
+		
+	}
 	
-		//创建面板
-		JPanel panel_queryCourse=new JPanel();
-		JPanel panel_listCourse=new JPanel();
-		JPanel panel_selectedCourse=new JPanel();
- 
- 	
-		panel_queryCourse.setLayout(null);
+	private void initPanelQueryCourse() {
+		panel_queryCourse.setLayout(null);	//绝对布局
 		
 		JLabel label_courseName = new JLabel("课程名称：");
 		label_courseName.setFont(new Font("宋体", Font.BOLD, 20));
@@ -82,12 +95,124 @@ public class PanelCourseSelect extends JPanel {
 		button_query.setFont(new Font("宋体", Font.BOLD, 20));
 		button_query.setBounds(460, 278, 181, 48);
 		panel_queryCourse.add(button_query);
-		
-		//将面板加入到选项卡面板对象上
-		tabbedPane.addTab("查询选课",null,panel_queryCourse,"First panel");
-		tabbedPane.addTab("开课列表",null,panel_listCourse,"Second panel");
-		tabbedPane.addTab("已选课程",null,panel_selectedCourse,"Third panel");
-		super.setSize(800, 470);
-		
 	}
+	
+	private void initPanelListCourse() {
+		panel_listCourse.setLayout(null);
+		
+		JLabel label_listCourse = new JLabel("开 课 列 表");
+		label_listCourse.setFont(new Font("宋体", Font.BOLD, 25));
+		label_listCourse.setBounds(349, 97, 226, 31);
+		panel_listCourse.add(label_listCourse);
+		
+		JButton button = new JButton("选课");
+		button.setBounds(91, 186, 72, 27);
+		panel_listCourse.add(button);
+		
+		JButton button_1 = new JButton("选课");
+		button_1.setBounds(91, 226, 72, 27);
+		panel_listCourse.add(button_1);
+		
+		JButton button_2 = new JButton("选课");
+		button_2.setBounds(91, 266, 72, 27);
+		panel_listCourse.add(button_2);
+		
+		JButton button_3 = new JButton("选课");
+		button_3.setBounds(91, 306, 72, 27);
+		panel_listCourse.add(button_3);
+		
+		JTable table_courseList = new JTable();
+		table_courseList.setFont(new Font("宋体", Font.BOLD, 20));
+		table_courseList.setBorder(new LineBorder(new Color(0, 0, 0)));
+		table_courseList.setRowHeight(40);//指定每一行的行高40
+		
+		table_courseList.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"课程编号", " 课程名", "  学分", "  时间", "  地点", "  备注", "考试时间"},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+			},
+			new String[] {
+					"课程编号", "课程名", "学分", "时间", "地点", "备注", "考试时间"
+			}
+		) {
+			boolean[] columnEditables = new boolean[] {
+				false, false, false, false, false, false, false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		table_courseList.getColumnModel().getColumn(0).setResizable(false);
+		table_courseList.getColumnModel().getColumn(1).setResizable(false);
+		table_courseList.getColumnModel().getColumn(2).setResizable(false);
+		table_courseList.setBounds(164, 141, 638, 200);
+		panel_listCourse.add(table_courseList);
+		
+		JTable table_background = new JTable();
+		table_background.setBounds(80, 86, 722, 255);
+		panel_listCourse.add(table_background);
+	}
+	
+	private void initPanelSelectedCourse() {
+		panel_selectedCourse.setLayout(null);
+		
+		JLabel label_selectedCourse = new JLabel("已 选 课 程");
+		label_selectedCourse.setFont(new Font("宋体", Font.BOLD, 25));
+		label_selectedCourse.setBounds(349, 97, 226, 31);
+		panel_selectedCourse.add(label_selectedCourse);
+		
+		JButton button = new JButton("退课");
+		button.setBounds(91, 186, 72, 27);
+		panel_selectedCourse.add(button);
+		
+		JButton button_1 = new JButton("退课");
+		button_1.setBounds(91, 226, 72, 27);
+		panel_selectedCourse.add(button_1);
+		
+		JButton button_2 = new JButton("退课");
+		button_2.setBounds(91, 266, 72, 27);
+		panel_selectedCourse.add(button_2);
+		
+		JButton button_3 = new JButton("退课");
+		button_3.setBounds(91, 306, 72, 27);
+		panel_selectedCourse.add(button_3);
+		
+		JTable table_courseSelected = new JTable();
+		table_courseSelected.setFont(new Font("宋体", Font.BOLD, 20));
+		table_courseSelected.setBorder(new LineBorder(new Color(0, 0, 0)));
+		table_courseSelected.setRowHeight(40);//指定每一行的行高40
+		
+		table_courseSelected.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"课程编号", " 课程名", "  学分", "  时间", "  地点", "  备注", "考试时间"},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+			},
+			new String[] {
+					"课程编号", "课程名", "学分", "时间", "地点", "备注", "考试时间"
+			}
+		) {
+			boolean[] columnEditables = new boolean[] {
+				false, false, false, false, false, false, false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		table_courseSelected.getColumnModel().getColumn(0).setResizable(false);
+		table_courseSelected.getColumnModel().getColumn(1).setResizable(false);
+		table_courseSelected.getColumnModel().getColumn(2).setResizable(false);
+		table_courseSelected.setBounds(164, 141, 638, 200);
+		panel_selectedCourse.add(table_courseSelected);
+		
+		JTable table_background = new JTable();
+		table_background.setBounds(80, 86, 722, 255);
+		panel_selectedCourse.add(table_background);
+	}
+	
 }
--- src/team/six/panel/homepage/PanelHomePage.java
+++ src/team/six/panel/homepage/PanelHomePage.java
@@ -4,12 +4,8 @@ import javax.swing.JPanel;
 
 public class PanelHomePage extends JPanel {
 
-	/**
-	 * Create the panel.
-	 */
 	public PanelHomePage() {
-		//JPanel panel_homePage=new JPanel();
-		//panel_homePage.setBounds(0, 0, 875, 468);
+
 	}
 
 }
