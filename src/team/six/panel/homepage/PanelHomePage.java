4147e4f83f147ea7c02d212a7b8e51aa8d16de9e
LiangJinH <1239144471@qq.com>
2019-05-08 14:30:52 +0800

    两斤好修改了一下日志事件

--- src/team/six/panel/homepage/PanelHomePage.java
+++ src/team/six/panel/homepage/PanelHomePage.java
@@ -393,8 +393,12 @@ public class PanelHomePage extends JPanel {
 		SchoolCardLossButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				log.info("饭卡丢了，赶紧挂失饭卡");
-				SchoolCardStatusLabel.setText("账号状态：挂失");
+				if(SchoolCardStatusLabel.getText().equals("账号状态：正常")) {
+					log.info("饭卡丢了，赶紧挂失饭卡");
+					SchoolCardStatusLabel.setText("账号状态：挂失");
+				}
+				else
+					log.warn("本来就是挂失的，还挂失什么");
 				HomePagePanel.updateUI();
 			}
 		});
