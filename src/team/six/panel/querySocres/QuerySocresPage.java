f35c9562aaa11e0c9933baec4f4b494b841b683d
denovetion <48473925+denovetion@users.noreply.github.com>
2019-05-08 08:47:41 +0800

    1

---
+++ src/team/six/panel/querySocres/QuerySocresPage.java
@@ -0,0 +1,16 @@
+package team.six.panel.querySocres;
+
+import javax.swing.JPanel;
+
+public class QuerySocresPage extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public QuerySocresPage() {
+
+		
+		
+	}
+
+}
